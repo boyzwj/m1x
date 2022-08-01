3507757069344d1866e2dd51efb7bc989c7e425c
Tom@ZhangWenjie <boyzwj@qq.com>
2022-08-01 20:09:04 +0800

    队伍逻辑

--- lib/m1x/role/mod/team.ex
+++ lib/m1x/role/mod/team.ex
@@ -1,4 +1,14 @@
 defmodule Role.Mod.Team do
-  defstruct role_id: nil,team_id: 0
+  defstruct role_id: nil, team_id: 0
   use Role.Mod
+
+  def h(~M{%M team_id} = _state, ~M{%Pbm.Team.Info2S }) do
+    if team_id == 0 do
+      info = %Pbm.Team.BaseInfo{team_id: 0}
+      %Pbm.Team.Info2C{info: info} |> sd()
+    else
+      ## TODO
+      :ok
+    end
+  end
 end
