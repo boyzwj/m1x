bed03bdc5d6579f5ce562969626da11867799665
slientgoat <409152092@qq.com>
2022-08-31 15:40:08 +0800

    remove repo

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,8 +30,6 @@ if config_env() == :prod do
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
-  config :m1x, M1x.Repo, database: "/tmp/.sqllite3/#{node()}/database.db"
-
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
