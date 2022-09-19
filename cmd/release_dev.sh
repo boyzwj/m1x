3e7f7f00c92c07ecfc57984e1c99edad377e1cfd
slientgoat <409152092@qq.com>
2022-09-19 15:39:41 +0800

    add NODE_NAME variable

--- cmd/release_dev.sh
+++ cmd/release_dev.sh
@@ -3,6 +3,10 @@ if [ ! -n "$REMOTE_HOST" ]; then
   REMOTE_HOST=tom@192.168.15.101
 fi
 
+if [ ! -n "$NODE_NAME" ]; then
+  NODE_NAME=develop
+fi
+
 REMOTE_PASS="1"
 
 echo "update server to $REMOTE_HOST"
@@ -17,9 +21,10 @@ echo 'upload to remote ....'
 sudo rsync -rltDvz --password-file=/etc/rsyncd.passwd _build/prod/rel/ $REMOTE_HOST::data
 echo '>>>>  upload finish  <<<<'
 echo 'begin restart server'
-sshpass -p "1" ssh -t $REMOTE_HOST "sudo RELEASE_NODE=develop /release/m1x/bin/m1x stop"
-sshpass -p "1" ssh -t $REMOTE_HOST "sudo PHX_SERVER=true RELEASE_NODE=develop /release/m1x/bin/m1x daemon"
+
+sshpass -p "1" ssh -t $REMOTE_HOST "sudo RELEASE_NODE=$NODE_NAME /release/m1x/bin/m1x stop"
+sshpass -p "1" ssh -t $REMOTE_HOST "sudo PHX_SERVER=true RELEASE_NODE=$NODE_NAME DB_PATH=\$HOME/db_data/$NODE_NAME /release/m1x/bin/m1x daemon_iex"
 echo 'begin restart dsa'
 sshpass -p "1" ssh -t $REMOTE_HOST "sudo RELEASE_NODE=dsa_1 /release/m1x/bin/m1x stop"
-sshpass -p "1" ssh -t $REMOTE_HOST "sudo RELEASE_NODE=dsa_1 DSA_PORT=20081 /release/m1x/bin/m1x daemon"
+sshpass -p "1" ssh -t $REMOTE_HOST "sudo RELEASE_NODE=dsa_1 DSA_PORT=20081 /release/m1x/bin/m1x daemon_iex"
 echo '>>>>  restart finish <<<<'
